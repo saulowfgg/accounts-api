6e4e64c7235d6df4e44d54829b92ca3875ec3008
Saulo Florencio Gomes <saulo.wfgg@gmail.com>
2022-03-28 20:19:55 -0300

    Modified Cors Settings

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
