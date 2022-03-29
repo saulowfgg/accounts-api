a58d067ddef90d36baacceb1584495bb308334f3
Saulo Florencio Gomes <saulo.wfgg@gmail.com>
2022-03-28 21:35:26 -0300

    Cors settings updated

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const configService = app.get(ConfigService)
+  const app = await NestFactory.create(AppModule);
+  const options = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "credentials":true
+  };
+  app.enableCors(options);
+  const configService = app.get(ConfigService);
   const PORT = configService.get('PORT') || 3000;
   await app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
