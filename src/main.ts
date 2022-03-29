78aa566c92bb58536fb28aeb4e50e22c404c0208
Saulo Florencio Gomes <saulo.wfgg@gmail.com>
2022-03-28 21:30:02 -0300

    Enviroment PORT varible update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  const configService = app.get(ConfigService)
+  const PORT = configService.get('PORT') || 3000;
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 bootstrap();
